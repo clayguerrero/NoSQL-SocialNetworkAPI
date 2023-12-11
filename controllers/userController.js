41aa219cabad10cf5b4d99d6e33a6775b9e854a4
clayg <claytondguerrero@gmail.com>
2023-12-10 19:38:36 -0600

    added routes for creating and deleting users

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,13 +12,38 @@ module.exports = {
 
   async getSingleUser(req, res) {
     try {
-        const user = await User.findOne({ _id: req.params.userId });
-        if (!user) {
-            return res.status(404).json({message: `No user found with that ID Number`})
-        }
-        res.json(user)
+      const user = await User.findOne({ _id: req.params.userId });
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: `No user found with that ID Number` });
+      }
+      res.json(user);
     } catch (error) {
       res.status(500).json({ message: `Cannot get user by ID` });
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: `Cannot create user` });
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: `No user found with that ID Number` });
+      }
+      res.json({ message: `User has been deleted` });
+    } catch (error) {
+      res.status(500).json({ message: `Cannot delete User` });
     }
   },
 };
